9ee0bdd591e6c2e0db521719eb2a8fa428c20842
Chikae <65932847+Chikae-Tatsumi@users.noreply.github.com>
2020-06-01 10:33:14 +0900

    Update 2-way_ANOVA.r

--- 2-way_ANOVA.r
+++ 2-way_ANOVA.r
@@ -20,8 +20,6 @@ for (i in 1:ncol(METADATA)){
 if (LT$`Pr(>F)`[-2]>0.05) {anova <- summary(aov(MODEL1))} else {anova <- summary(aov(MODEL2))}
 Fval <- anova[[1]]$`F value`
 Pval <- anova[[1]]$`Pr(>F)`
-Fval <- Fval[-length(Fval)]
-Pval <- Pval[-length(Pval)]
 Bind <- c(Fval[1], Pval[1], Fval[2], Pval[2], Fval[3], Pval[3])
 names(Bind) <- c(paste(NAME1,".F",sep=""),paste(NAME1,".P",sep=""),paste(NAME2,".F",sep=""),paste(NAME2,".P",sep=""),paste(NAME1,"*",NAME2,".F",sep=""),paste(NAME1,"*",NAME2,".P",sep=""))
 Results <- rbind(Results, Bind)}
